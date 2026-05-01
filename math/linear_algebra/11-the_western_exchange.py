f9d3199971459def8431a230db13626d367f0739
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-01 21:49:28 +0000

    make

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that transposes a numpy ndarray"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """Returns the transpose of a numpy ndarray"""
+    return matrix.T
