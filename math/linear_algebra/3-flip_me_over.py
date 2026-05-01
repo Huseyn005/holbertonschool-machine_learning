7b7f8348ac0af629faf2340c6f26969feb626aca
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-01 21:25:47 +0000

    make

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,4 +6,3 @@ def matrix_transpose(matrix):
     """Returns the transpose of a 2D matrix"""
     return [[matrix[row][col] for row in range(len(matrix))]
             for col in range(len(matrix[0]))]
-            
