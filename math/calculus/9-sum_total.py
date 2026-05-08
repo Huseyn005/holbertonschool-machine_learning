74a6a5e5d71e671864f9084f6d47fda50cdc070a
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-08 19:21:33 +0000

    Add Task 9: Summation i squared without loops

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module to calculate the sum of i squared"""
+
+
+def summation_i_squared(n):
+    """
+    Calculates the sum of i^2 from 1 to n
+    Returns the integer value of the sum, or None if n is not valid
+    """
+    if not isinstance(n, int):
+        return None
+    if n < 0:
+        return None
+    
+    # Using the formula n(n+1)(2n+1) / 6 to avoid loops
+    return (n * (n + 1) * (2 * n + 1)) // 6
