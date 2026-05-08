bd3675f3fb417d2ae8f29cd7ccd6fa65ece9df48
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-08 19:32:53 +0000

    Fix pycodestyle and invalid input return

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,11 +3,7 @@
 
 
 def summation_i_squared(n):
-    """
-    Calculates the sum of i^2 from 1 to n
-    Returns the integer value of the sum, or None if n is not valid
-    """
+    """Calculates the sum of i^2 from 1 to n"""
     if not isinstance(n, int) or n < 0:
         return None
-
     return (n * (n + 1) * (2 * n + 1)) // 6
