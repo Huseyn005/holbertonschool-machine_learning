a75e80175b211ad20865aa56d73031d0b2827005
Hn005 <huseyn.mammadli.05@gmail.com>
2026-05-11 20:21:38 +0000

    task4

---
+++ math/probability/normal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module for Normal distribution"""
+
+
+class Normal:
+    """Class that represents a normal distribution"""
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initialize Normal distribution"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+            variance = sum([(x - self.mean) ** 2 for x in data]) / len(data)
+            self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return (z * self.stddev) + self.mean
