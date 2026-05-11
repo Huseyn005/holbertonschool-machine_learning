e53ef9159a04fa2c52d927d40c365761afafca85
Hn005 <huseyn.mammadli.05@gmail.com>
2026-05-11 20:37:01 +0000

    task4

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -4,7 +4,6 @@
 
 class Binomial:
     """Class that represents a binomial distribution"""
-
     def __init__(self, data=None, n=1, p=0.5):
         """Initialize Binomial distribution"""
         if data is None:
