30ca6981d8af3e4c9aaf354e5258cdda72616700
Hn005 <huseyn.mammadli.05@gmail.com>
2026-05-11 19:55:01 +0000

    task 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
-"""
-Module for Poisson distribution
-"""
+""" Module for Poisson distribution """
 
 
-def pmf(self, k):
-        """
-        Calculates the value of the PMF for a given number of 'successes'
-        """
-        # Convert k to an integer
+class Poisson:
+    """ Class that represents a poisson distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        """ Initialize Poisson distribution """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """ Calculates the value of the PMF for a given number of 'successes' """
         k = int(k)
-
-        # If k is out of range, return 0
         if k < 0:
             return 0
 
-        # e is approximately 2.7182818285
+        # Euler's number
         e = 2.7182818285
         
-        # Calculate factorial of k
+        # Factorial calculation
         factorial = 1
         for i in range(1, k + 1):
             factorial *= i
 
-        # PMF Formula: (e^-lambda * lambda^k) / k!
-        pmf_value = (e ** -self.lambtha) * (self.lambtha ** k) / factorial
-        
-        return pmf_value
-
+        # P(X = k) = (e^-lambda * lambda^k) / k!
+        return (e ** -self.lambtha) * (self.lambtha ** k) / factorial
