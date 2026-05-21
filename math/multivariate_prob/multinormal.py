575d1824c69adbe606786de9922ae06484948407
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-21 11:07:50 +0000

    task

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -15,6 +15,7 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         data_centered = data - self.mean
         self.cov = np.matmul(data_centered, data_centered.T) / (n - 1)
+        
     def pdf(self, x):
         """Calculates the PDF at a specific data point"""
         if not isinstance(x, np.ndarray):
