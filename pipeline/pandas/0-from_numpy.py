0f0349a33cb26cd9ce97a405e162f65b0dc8a862
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-27 20:36:00 +0000

    task

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,2 +1,29 @@
-git config --global user.name "Huseyn005"
-git config --global user.email "huseyn.mammadli.05@gmail.com"
+#!/usr/bin/env python3
+"""
+Defines a function that creates a pd.DataFrame from a np.ndarray
+"""
+import pandas as pd
+
+
+def from_numpy(array):
+    """
+    Creates a pd.DataFrame from a np.ndarray with alphabetical column labels.
+
+    Args:
+        array (np.ndarray): The numpy array to convert.
+
+    Returns:
+        pd.DataFrame: The newly created DataFrame.
+    """
+    # Get the number of columns in the numpy array
+    num_cols = array.shape[1]
+
+    # Generate alphabet list: ['A', 'B', 'C', ..., 'Z'] up to num_cols
+    # ASCII value for 'A' is 65
+    columns = [chr(65 + i) for i in range(num_cols)]
+
+    # Alternatively, you could use:
+    # import string
+    # columns = list(string.ascii_uppercase[:num_cols])
+
+    return pd.DataFrame(array, columns=columns)
