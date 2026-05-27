990682c832ea1e1544841f3d2df4666b8f50bcfd
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-27 21:44:20 +0000

    task

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""Module to index and concatenate two separate financial DataFrames."""
+"""
+Module to index and concatenate two separate financial DataFrames.
+"""
 import pandas as pd
+index = __import__('10-index').index
 
 
 def concat(df1, df2):
-    """Index dataframes, filter rows, and merge them with multi-keys."""
-    index_func = __import__('10-index').index
-    df1 = index_func(df1)
-    df2 = index_func(df2)
-    df2_filtered = df2.loc[:1417411920]
+    """
+    Indexes dataframes, filters down rows, and merges them with multi-keys.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+    df2_filtered = df2.loc[df2.index <= 1417411920]
     return pd.concat([df2_filtered, df1], keys=['bitstamp', 'coinbase'])
