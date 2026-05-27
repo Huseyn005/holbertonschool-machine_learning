61e19cd9100d9ff01980242b9196b8153bf1d091
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-27 21:21:13 +0000

    task

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -23,19 +23,20 @@ def hierarchy(df1, df2):
     df1_indexed = index(df1)
     df2_indexed = index(df2)
 
-    # 2. Slice both dataframes to include only timestamps from
-    # 1417411980 to 1417417980 inclusive
+    # 2. Slice both dataframes between the target timestamps inclusive
     df1_filtered = df1_indexed.loc[1417411980:1417417980]
     df2_filtered = df2_indexed.loc[1417411980:1417417980]
 
-    # 3. Concatenate bitstamp on top of coinbase with structural keys
-    df = pd.concat([df2_filtered, df1_filtered], keys=['bitstamp', 'coinbase'])
+    # 3. Concatenate (broken into shorter lines to pass PEP 8 < 80 chars)
+    df = pd.concat(
+        [df2_filtered, df1_filtered],
+        keys=['bitstamp', 'coinbase']
+    )
 
-    # 4. Rearrange MultiIndex levels so Timestamp is level 0 and keys are level 1
-    # .swaplevel(0, 1) switches their order positions
+    # 4. Rearrange MultiIndex levels so Timestamp is level 0
     df = df.swaplevel(0, 1)
 
-    # 5. Sort the data chronologically by index (Timestamp first)
+    # 5. Sort the data chronologically by index
     df = df.sort_index()
 
     return df
