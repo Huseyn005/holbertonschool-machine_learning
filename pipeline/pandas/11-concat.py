e4cc29aaa8b3ebdb718e7e592ef599a8807a635b
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-27 21:13:55 +0000

    task

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -2,6 +2,7 @@
 """
 Defines a function that concatenates specific slices of two pd.DataFrames
 """
+import pandas as pd
 index = __import__('10-index').index
 
 
@@ -17,19 +18,15 @@ def concat(df1, df2):
     Returns:
         The concatenated DataFrame with hierarchical indexing.
     """
-    # 1. Index both dataframes on their Timestamp columns using task 10 function
+    # Index both dataframes using the imported index function
     df1_indexed = index(df1)
     df2_indexed = index(df2)
 
-    # 2. Slice df2 to include all rows up to and including timestamp 1417411920
-    # Since it is indexed on Timestamp, we can use label-based slicing via .loc
+    # Filter bitstamp data up to and including timestamp 1417411920
     df2_filtered = df2_indexed.loc[:1417411920]
 
-    # 3. Concatenate df2_filtered to the top of df1_indexed
-    # We pass [df2_filtered, df1_indexed] to put bitstamp on top.
-    # The 'keys' parameter adds the multi-index labeling required.
-    # We call the .concat method from df1's underlying class to avoid imports.
-    return df1.__class__.concat(
+    # Concatenate df2 on top of df1 with specified multi-index keys
+    return pd.concat(
         [df2_filtered, df1_indexed],
         keys=['bitstamp', 'coinbase']
     )
