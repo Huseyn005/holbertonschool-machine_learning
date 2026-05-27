8f15aba66c36cb03992bbf758dda5629dce23634
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-27 22:47:20 +0000

    task

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -12,24 +12,24 @@ def bars():
 
     # Define names for X-axis ticks
     people = ['Farrah', 'Fred', 'Felicia']
- 
+
     # Define fruit types, their hex colors, and tracking for the stack bottom
     fruit_types = ['apples', 'bananas', 'oranges', 'peaches']
     colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
- 
+
     # Initialize the bottom of the stack at zero for each person
     bottoms = np.zeros(3)
- 
+
     # Plot each row (fruit) stacked on top of the previous ones
     for i in range(len(fruit)):
         plt.bar(people, fruit[i], width=0.5, bottom=bottoms,
                 color=colors[i], label=fruit_types[i])
         bottoms += fruit[i]   # Update the bottom position
-    
+
     # Set the title and labels
     plt.title('Number of Fruit per Person')
     plt.ylabel('Quantity of Fruit')
-  
+
     # Set Y-axis range and step ticks
     plt.ylim(0, 80)
     plt.yticks(np.arange(0, 81, 10))
